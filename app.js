822dc9ad1344f436c555a8f5eae98f2e199f0bae
SimoneDessi <simonedessi1984@gmail.com>
2023-07-17 10:48:29 +0200

    allowws cross origin resource sharing

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ const {
   handleCustomErrors,
   handleServerErrors,
 } = require("./errors/errors.js");
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/topics", getTopics);
 
